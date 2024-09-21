f74384cb71f1cf1a790c7fa3426a8375a7040f30
Namayjain <namayjain2@gmail.com>
2024-09-21 17:58:35 +0530

    first commit

---
+++ src/pages/NotesPage.jsx
@@ -0,0 +1,32 @@
+import NoteCard from "../components/NoteCard"
+import { useContext } from "react";
+import { NoteContext } from "../context/NoteContext";
+import Controls from "../components/Controls";
+
+
+const NotesPage = () => {
+  const { notes, setNotes } = useContext(NoteContext);
+  // const [notes, setNotes] = useState([]);
+ 
+  //   useEffect(() => {
+  //       init();
+  //   }, []);
+ 
+  //   const init = async () => {
+  //       const response = await db.notes.list();
+  //       setNotes(response.documents);
+  //   };
+
+
+  return (
+    <div>
+      {notes.map((note)=>(
+        <NoteCard note={note} key={note.$id} />
+      ))}
+      <Controls/>
+    </div>
+
+  )
+}
+
+export default NotesPage
